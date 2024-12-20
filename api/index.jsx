5a314cbfdd98b280fa63252957f0290f8f1c9da5
sammeddev <sammed.hunchimare@buddyloan.com>
2024-12-20 11:34:07 +0530

    add: log added

--- api/index.jsx
+++ api/index.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 // Set the base URL of the backend API
 const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API;
 
+console.log(BASE_URL);
+
 // Create an Axios instance with the base URL
 const api = axios.create({
   baseURL: BASE_URL,
